dfa40a2532bdbae5fa2ab84aeb76fc1debf73bf5
Roshanshres <roshanshrestha992@gmail.com>
2024-03-10 17:21:14 +0545

    wrapper

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -3,10 +3,10 @@ from django.shortcuts import redirect
 
 
 def unauthenticated_user(view_func):
-    def wrappper_func(request, *args, **kwargs):
+    def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('home')
         else:   
             return view_func(request, *args, **kwargs)
     
-    return wrappper_func
+    return wrapper_func
